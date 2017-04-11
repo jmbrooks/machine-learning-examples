9e7cbf2884d9b8ad7cc96f696d915da3184cbe48
jmbrooks <jbrooks93@gmail.com>
2017-04-11 00:45:34 -0700

    first: quandl connected df

---
+++ intro.py
@@ -0,0 +1,7 @@
+# Machine learning introduction
+# Move from regression to deep learning with TensorFlow
+# Plan: Basics first, let's hit the concepts and the core ML algorithms
+# From there, we'll move to implementation, demonstrate with scikit-learn
+# Finally, we'll use outside tools, TensorFlow, AWS, to implement and
+# Use ML algorithms, connect to and deploy APIs
+# Stick to it! Will need sk-learn, quandl, pandas to start
