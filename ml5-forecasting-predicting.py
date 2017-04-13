f905dfe2e79ed160b40fa1a601e5f70c6b071afd
jmbrooks <jbrooks93@gmail.com>
2017-04-12 23:52:37 -0700

    Imported ggplot for visualization

--- ml5-forecasting-predicting.py
+++ ml5-forecasting-predicting.py
@@ -1,40 +1,58 @@
 import quandl, math
 import numpy as np
 import pandas as pd
-from sklearn import preprocessing, cross_validation, svm  # Note preprocessing for scaling, feature scalars between 0, 1
+from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+import datetime
+import pickle
 
-df = quandl.get('WIKI/GOOGL')
+style.use('ggplot')
 
-df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume', ]]  # Grab label and (potential) features
-df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0  # Define important custom vars
+df = quandl.get("WIKI/GOOGL")
+df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
-df = df[['Adj. Close', 'HL_PCT', 'PCT_changecmd', 'Adj. Volume']]  # Update data frame to match y = mx...+b format
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+forecast_col = 'Adj. Close'
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.1 * len(df)))
 
-forecast_col = 'Adj. Close'  # Use this to define forecasting column, can reuse general format for different apps
-df.fillna(-99999, inplace=True)  # Fill any NA values with an outlier value
-
-forecast_out = int(math.ceil(0.01*len(df)))  # project 10% out from the data frame
-
-df['label'] = df[forecast_col].shift(-forecast_out)  # Shifts out for projection
+df['label'] = df[forecast_col].shift(-forecast_out)
 
 X = np.array(df.drop(['label'], 1))
-X = X[:-forecast_out]
+X = preprocessing.scale(X)
 X_lately = X[-forecast_out:]
-X = preprocessing.scale(X)  # Need to scale new values alongside all others values for corr preprocessing
+X = X[:-forecast_out:]
 
-df.dropna(inplace=True)  # Drops any NA (outlier) values
-y = np.array(df['label'])
+df.dropna(inplace=True)
+
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-clf = LinearRegression(n_jobs=1)  # Support vector regression...can easily switch algorithms, back to LinearRegression
+clf = svm.SVR(kernel='linear')
 clf.fit(X_train, y_train)
-
-accuracy = clf.score(X_test, y_test)  # squared error = accuracy
+confidence = clf.score(X_test, y_test)
+print(confidence)
 
 forecast_set = clf.predict(X_lately)
+df['Forecast'] = np.nan
 
-print(forecast_set, accuracy, forecast_out)
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
